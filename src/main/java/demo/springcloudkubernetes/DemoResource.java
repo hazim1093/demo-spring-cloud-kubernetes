cd484f182013dceb9142d2e733e1af88936939f6
Hazim Malik <hazim_malik@hotmail.com>
2018-09-10 16:04:37 +0200

    Add configuration and code for reading configmap value

---
+++ src/main/java/demo/springcloudkubernetes/DemoResource.java
@@ -0,0 +1,18 @@
+package demo.springcloudkubernetes;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoResource {
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
+    @GetMapping
+    public String getGreetingMessage(){
+        return "Greeting Message: " + greetingMessage;
+    }
+}
