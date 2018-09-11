65e41c6f8c46f51b99cca4bbbc3ebff52daa29be
Hazim Malik <hazim_malik@hotmail.com>
2018-09-11 13:04:30 +0200

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8-alpine
 ARG VERSION=0.1.0
-ADD target/springcloudkubernetes-${VERSION}.jar /springcloudkubernetes.jar
+ADD springcloudkubernetes-${VERSION}.jar /springcloudkubernetes.jar
 CMD java -jar /springcloudkubernetes.jar
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+set -e
 
-docker build -t hazim/springcloudkubernetes .
-docker push hazim/springcloudkubernetes
-
-kubectl apply -f src/kubernetes -n test
+mvn package
+cp target/springcloudkubernetes-*.jar out/
+docker build -f Dockerfile -t hazim/demo-springcloudkubernetes out/
+docker push hazim/demo-springcloudkubernetes
+kubectl --kubeconfig /c/config/config-devtest-apps187 apply -f src/kubernetes -n cp
